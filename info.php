1d71e8e78ed51b02bf8221ab340627859603551a
urbaniak <urbaniaknicolas62@gmail.com>
2017-09-08 09:32:38 +0200

    creation des liens header

--- info.php
+++ info.php
@@ -2,9 +2,13 @@
 
  /*-------------HEADER-------------------*/
  $infos=[
-   'header' => ['titre'=>'Notre site E-commerce',
+   'header' => ['titre'  =>'Notre site E-commerce',
                 'product'=>'produit par',
-                'nom'=>'Leila & Nicolas'],
+                'nom'    =>'Leila & Nicolas',
+                'desc'   =>'blablablabla',
+                'lien1'  =>'Notre entreprise',
+                'lien2'  =>'Nos promotions',
+                'lien3'  =>'contact'],
   'footer' => []
 
  ];
