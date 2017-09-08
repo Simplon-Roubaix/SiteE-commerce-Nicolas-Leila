52dcc513e02e3a8e323fc09600c19d6e4284bf34
urbaniak <urbaniaknicolas62@gmail.com>
2017-09-08 13:50:08 +0200

    fiche produit boucle foreach

--- index.php
+++ index.php
@@ -16,13 +16,12 @@ foreach ($produits as $key => $value) {
          <div class="card-image">
            <img src="<?php echo $value['image']; ?>">
            <span class="card-title v"></span>
+           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">+</i></a>
+
          </div>
          <div class="card-content">
            <p> <?php echo $value['description']; ?></p>
            <p> <?php echo 'prix '.$value['prix']; ?></p>
-         </div>
-         <div class="card-action">
-           <a href="#">This is a link</a>
          </div>
        </div>
      </div>
