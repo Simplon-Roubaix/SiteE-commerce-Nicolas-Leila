76aceb86372f5c68c665d98c0f57f0f18bfa3854
urbaniak <urbaniaknicolas62@gmail.com>
2017-09-19 09:55:19 +0200

    creation formulaire

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+include('header.php');
+?>
+<div id="form" class="container">
+<div class="row">
+    <form class="col s10" action="login_post.php" method="post">
+        <div class="input-field col s6">
+          <input id="first_name" name="pseudo" type="text" class="validate">
+          <label for="first_name">Pseudo</label>
+        </div>
