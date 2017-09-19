1351532710091bc83117f3999fccc26dad89d957
urbaniak <urbaniaknicolas62@gmail.com>
2017-09-19 09:51:55 +0200

    requete

--- index.php
+++ index.php
@@ -24,3 +24,32 @@ include('header.php');
     $reponse = $bdd->query('SELECT * FROM cartes, image WHERE image.id_cartes = cartes.id');
       while ($donnees = $reponse->fetch()) {
         ?>
+        <div id="carte" class="col s12 m6 l6 xl6">
+      <div class="card horizontal card small">
+        <div class="card-image">
+          <img src="<?php echo $donnees['chemin']; ?>" alt="meuble">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <p><?php echo $donnees['description']; ?><br>
+             <?php echo $donnees['prix']; ?> €</p>
+          </div>
+          <div class="card-action">
+            <a href="pageProduit.php?join=<?php echo $donnees['id']; ?>">Voir le produit</a>
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <?php
+  }
+  $reponse->closeCursor();
+  ?>
+
+  </div>
+  </div>
+
+ <?php
+ include('phpPages/footer.php');
+  ?>
